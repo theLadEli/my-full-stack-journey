ae2079afea15151328f25c0c66dd29719c30f602
Eli <109231330+theLadEli@users.noreply.github.com>
2022-11-14 15:41:09 +0000

    Saved solution to 99 Bottles on The Wall

---
+++ JavaScript/99 Bottles on The Wall/index.js
@@ -0,0 +1,21 @@
+var bottles = 100;
+
+function lyrics() {
+
+    while (bottles  <= 100){
+        
+        bottles--;
+        
+        if (bottles <= 99 && bottles > 1){
+            console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer. Take 1 down, pass it around, " + bottles + " of beer on the wall.")
+        } else if (bottles === 1){
+            console.log(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer. Take 1 down, pass it around, no more bottles of beer on the wall.")
+        } else if (bottles === 0) {
+            console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
+        }
+
+    }
+    
+}
+
+lyrics();
