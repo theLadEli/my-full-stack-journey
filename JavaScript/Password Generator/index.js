2ca1168ede71e52ae64a908eed9d62e4dace534b
Eli <109231330+theLadEli@users.noreply.github.com>
2023-01-13 14:17:52 +0000

    Update index.js
    Now it generates a password

--- JavaScript/Password Generator/index.js
+++ JavaScript/Password Generator/index.js
@@ -1,19 +1,22 @@
 // List of all characters to choose from
-var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?", ".", ",", "$", "£"];
+var characters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?", ".", ",", "$", "£"];
 
 // Getting a random character from the password array
-var randomCharacter = characters[Math.floor((Math.random() * characters.length) + 1)];
 
 
 // Getting a random length of password, minimum of eight characters
 var length = Math.floor((Math.random() * 8) + 8);
 
+// Creating the array for the password
 var password = [];
 
-
-for (let i = 0; i == length; i++) {
-    password.push(randomCharacter)
+// A loop to generate the amount of characters equal to the amount of a random number larger than 8
+for (let i = 0; i < length; i++) {
+    var randomCharacter = characters[Math.floor((Math.random() * characters.length))];
+    password.push(randomCharacter);
 }
 
-// password.push(randomCharacter)
-console.log(password)
+// The generated password:
+var strongPass = password.join('');
+
+console.log(strongPass);
