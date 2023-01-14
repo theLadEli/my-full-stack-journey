c947e6782bc97f267a09abf2baae4efc9e439e5a
Eli <109231330+theLadEli@users.noreply.github.com>
2023-01-14 23:25:06 +0000

    Copy and Refresh buttons functional

--- JavaScript/Password Generator/index.js
+++ JavaScript/Password Generator/index.js
@@ -2,9 +2,6 @@
 // List of all characters to choose from
 var characters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?", ".", ",", "$", "£"];
 
-// Getting a random character from the password array
-
-
 // Getting a random length of password, minimum of eight characters
 var length = Math.floor((Math.random() * 14) + 8);
 
@@ -23,3 +20,13 @@ var strongPass = password.join('');
 
 // document.getElementById('p').setInnerText = strongPass;
 document.querySelector('p').innerHTML = strongPass;
+
+function copyToClipboard() {
+
+    // Select the text field
+    // document.querySelector('h1').innerHTML.select();
+    // copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(strongPass);
+}
