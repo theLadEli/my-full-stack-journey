d519c72019c52fc475507b719aab81358d4c4d30
Eli <109231330+theLadEli@users.noreply.github.com>
2023-04-03 21:47:03 +0100

    Update index.js

--- JavaScript/Drum Kit/index.js
+++ JavaScript/Drum Kit/index.js
@@ -1,55 +1,15 @@
+var count = 0;
+var numberofDrums = document.querySelectorAll(".drum").length;
 
-var numberOfDrums = document.querySelectorAll(".drum").length;
+while (count < numberofDrums ) {
 
-for (var count = 0; count < numberOfDrums; count++) {
+    //Adds an event listener to the first button, it listens for a click and then calls the onButtonClick() function - Didn't add parenthesis after the function as we don't want the function to be executed when the event listener is added
+    document.querySelectorAll(".drum")[count].addEventListener("click", onButtonClick);
 
-    document.querySelectorAll(".drum")[count].addEventListener("click", function(){
-
-        var buttonInnerHTML = this.innerHTML;
-
-            switch (buttonInnerHTML) {
-                case "w":
-                    var crash = new Audio('sounds/crash.mp3');
-                    crash.play();             
-                    break;
-
-                case "a":
-                var kick = new Audio('sounds/kick-bass.mp3');
-                kick.play(); 
-                break;
-
-                case "s":
-                var snare = new Audio('sounds/snare.mp3');
-                snare.play(); 
-                break;
-
-                case "d":
-                var tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play(); 
-                break;
-
-                case "j":
-                var tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play(); 
-                break;
-
-                case "k":
-                var tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play(); 
-                break;
-
-                case "l":
-                var tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play(); 
-                break;
+    count++;
+}
 
 
-                default: console.log(buttonInnerHTML)
-            }
-
-
-        // console.log(this.innerHTML);
-
-    });
-
-}  
+function onButtonClick() {
+    alert("I got clicked!")
+}
