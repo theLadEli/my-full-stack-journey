218a5950de0f9f0cfd72114828d71935ec18693b
Eli <109231330+theLadEli@users.noreply.github.com>
2023-06-01 12:01:05 +0100

    Cleaned up move data

--- Python/Lab 03/movie_data.py
+++ Python/Lab 03/movie_data.py
@@ -1,7 +1,8 @@
 file_path = r"C:\Users\cohen\Documents\GitHub\my-full-stack-journey\Python\Lab 03\top250_movies.txt"
+file = open(file_path, "tr")
 count = 0
 
-with open(file_path, "tr") as file:
+with file as file:
     for line in file:
         count+=1
         print(f"{count}. {line}")
