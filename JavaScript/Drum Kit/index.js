7c812ab308f9c3399c45d694b843de0e4c11ae4a
Eli <109231330+theLadEli@users.noreply.github.com>
2023-07-16 19:55:25 +0100

    Finished the Drum Kit

--- JavaScript/Drum Kit/index.js
+++ JavaScript/Drum Kit/index.js
@@ -1,52 +1,68 @@
-function playSound(){
-    var selectedButton = this.innerHTML;
+var NumberOfDrums = document.getElementsByClassName("drum-item").length;
 
-    switch (selectedButton) {
-        case "W":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+// Detecting Button Press
+for (let i = 0; i < NumberOfDrums; i++) {
+    document.getElementsByClassName("drum-item")[i].addEventListener("click", function() {
+        
+        var selectedButton = this.innerHTML;
+        makeSound(selectedButton);
+        drumButtonAnimation(selectedButton);
+
+    });
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keydown", function(){
+
+    makeSound(event.key.toLowerCase());
+    drumButtonAnimation(event.key);
+    
+})
+
+// Play the Function
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
 
         case "a":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
             
         case "s":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
                         
         case "d":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
                         
         case "j":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
                         
         case "k":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
 
         case "l":
-            new Audio(`sounds/${selectedButton}-drum-sound.mp3`).play();
+            new Audio(`sounds/${key}-drum-sound.mp3`).play();
             break;
             
         default:
             break;
     }
+
 }
 
-var NumberOfDrums = document.getElementsByClassName("drum-item").length;
+function drumButtonAnimation(currentKey) {
 
-for (let i = 0; i < NumberOfDrums; i++) {
-    document.getElementsByClassName("drum-item")[i].addEventListener("click", playSound);
-}
+    document.querySelector(`.${currentKey}`).classList.add("pressed");
 
-document.addEventListener("keydown", function(){
-    let key = event.key;
-
-    if (key == "w"){
-        alert("W was pressed.")
-    } else {
-        alert("Something was pressed.")
+    setTimeout(function(){
+        document.querySelector(`.${currentKey}`).classList.remove("pressed");
     }
-})
+        ,500)
+}
