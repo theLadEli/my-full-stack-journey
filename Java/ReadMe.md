90fc642897da0ce25564bd1628a8014ec4db8b2b
Eli <109231330+theLadEli@users.noreply.github.com>
2023-10-22 19:09:40 +0100

    Completed the Simon Says Game

---
+++ Java/ReadMe.md
@@ -0,0 +1,18 @@
+#Java
+
+# About Java
+
+### Why was Java invented & What is it used for?
+Typical progrem like `C` needs to be compiled specifically for OS's.. 
+
+### JVM
+
+### JDK
+
+### JRE
+
+### What is Java used for
+ - One of the main usecases is Android.
+ - IoT (internet of things)
+ - Web applications (backend)
+ - Interacting with Databases
--- JavaScript/Simon Game Challenge Starting Files/game.js
+++ JavaScript/Simon Game Challenge Starting Files/game.js
@@ -1,63 +1,90 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
+
 var gameStarted = false;
 var level = 0;
 
-$(document).on("keydown", function(){
+$(document).on("keypress", function() {
+
     if (gameStarted == false) {
         nextSequence();
         gameStarted = true;
-    } else {
-        return;
     }
-})
-
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*4);
-    randomChosenColour = buttonColours[randomNumber];
-
-    gamePattern.push(randomChosenColour);
-    console.log(gamePattern)
-
-    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour);
-    level++;
-    
-    $("h1").text(`Level ${level}`);
-};
-
-$(".btn").click (function(){
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-
-    console.log(userClickedPattern);
-    checkAnswer();
 });
 
-function playSound(soundName){
-    new Audio(`sounds/${soundName}.mp3`).play();
+$(".btn").on("click", function() {
+    
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour)
+
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
+
+    checkAnswer(userClickedPattern.length-1)
+
+});
+
+function nextSequence() {
+    
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text(`Level ${level}`)
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour)
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
 }
 
-function animatePress(currentColour) {
-    $(`#${currentColour}`).addClass("pressed")
+function playSound(name) {
+    
+    var audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
 
-    setTimeout(function (){
-        $(`#${currentColour}`).removeClass("pressed")          
-    }, 100); 
+}
+
+function animatePress(currentColour){
+    $(`#${currentColour}`).addClass("pressed");
+
+    setTimeout(function() {
+        $(`#${currentColour}`).removeClass("pressed");
+    }, 100);
 }
 
 function checkAnswer(currentLevel) {
 
-    console.log(`Game Pattern: ${gamePattern}\nUser Pattern: ${userClickedPattern}`)
-    nextSequence()
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+        console.log("Success.")
 
-    // if (userClickedPattern == gamePattern) {
-    //     nextSequence()
-    // } else {
-    //     alert("Wrong.")
-    // }
+        if (userClickedPattern.length === gamePattern.length) {
 
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart")
+
+        startOver();
+    }
+
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
 }
